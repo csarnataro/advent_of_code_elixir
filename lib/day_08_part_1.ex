02f178d037b544038c14f17bce07d25068b85301
Christian Sarnataro <csarnataro@users.noreply.github.com>
2022-12-22 23:53:33 +0100

    Day 08 optimizations (#1)
    Removed multiple matrix transpositions.
    Now it takes around 0.2 sec to complete.

--- lib/day_08_part_1.ex
+++ lib/day_08_part_1.ex
@@ -1,4 +1,20 @@
 defmodule Day08Part1 do
+
+    # NOTES about optimizations
+    #
+    # It took 4.48619 seconds to get the result, with the initial naive approach
+    # How long will it take with some optimization?
+    #
+    # step 1: with
+    #         - for comprehension
+    #         - skipping double matrix transposition for each element
+    #         - filter results and counting them instead of adding them to the full matrix and then reducing it
+    # It took 0.204323 seconds to get the result (~20x faster)
+    #
+    # step 2: maybe adding multiprocess?
+    #
+    #
+
   def sample do
     content = File.read!("data/day_08/sample.txt")
     doit(content)
@@ -6,7 +22,9 @@ defmodule Day08Part1 do
 
   def puzzle do
     content = File.read!("data/day_08/puzzle.txt")
-    doit(content)
+    {uSecs, result} = :timer.tc(Day08Part1, :doit, [content])
+    IO.puts("It took #{uSecs / 1_000_000} seconds to get the result")
+    result
   end
 
   def get(grid, x, y) do
@@ -27,38 +45,28 @@ defmodule Day08Part1 do
   def doit(input) do
     grid =
       String.split(input, "\n")
-      |> Enum.map(fn line -> String.graphemes(line) end)
+      |> Enum.map(&String.graphemes/1)
       |> Enum.map(&Enum.map(&1, fn v -> String.to_integer(v) end))
 
-    num_rows = length(grid)
-    num_cols = length(List.first(grid))
+    transposed = transpose_matrix(grid)
 
-    protection_levels =
-      grid
-      |> Enum.with_index()
-      |> Enum.map(fn {row, y} ->
-        row
-        |> (&Range.new(0, length(&1) - 1)).()
-        |> Enum.map(fn x -> protection_level(grid, x, y) end)
-      end)
+    num_elems = length(grid) * length(List.first(grid))
 
-    num_rows * num_cols -
-      Enum.reduce(protection_levels, 0, fn row, acc ->
-        acc +
-          (row
-           |> Enum.filter(fn v -> v == 4 end)
-           |> Enum.count())
-      end)
+    for {row, y} <- Enum.with_index(grid),
+        {_elem, x} <- Enum.with_index(row), reduce: 0 do
+          acc -> if protection_level(grid, transposed, x, y) == 4, do: acc + 1, else: acc # "#{x} x #{y}" # ==
+    end
+    |> (&Kernel.-(num_elems, &1)).()
   end
 
-  def protection_level(grid, x, y) do
+  def protection_level(grid, transposed_grid, x, y) do
     if x == 0 || x == length(List.first(grid)) - 1 || y == 0 || y == length(grid) - 1 do
       0
     else
       is_protected(grid, x, y, :left) +
         is_protected(grid, x, y, :right) +
-        is_protected(grid, x, y, :bottom) +
-        is_protected(grid, x, y, :top)
+        is_protected(transposed_grid, x, y, :bottom) + # <=== use the transposed grid here
+        is_protected(transposed_grid, x, y, :top)      # <=== and here
     end
   end
 
@@ -93,10 +101,10 @@ defmodule Day08Part1 do
   end
 
   def is_protected(grid, x, y, :top) do
-    is_protected(transpose_matrix(grid), y, x, :left)
+    is_protected(grid, y, x, :left) # uses the transposed grid
   end
 
   def is_protected(grid, x, y, :bottom) do
-    is_protected(transpose_matrix(grid), y, x, :right)
+    is_protected(grid, y, x, :right) # uses the transposed grid
   end
 end
